c852be072692034cac69c33d04307c5e33b6262b
Rubenpetina <r.petina@studenti.unisa.it>
2023-07-23 13:13:59 +0200

    risoluzione errore

--- GameChanger/src/main/java/cn/gamechanger/model/dao/AccessorioDao.java
+++ GameChanger/src/main/java/cn/gamechanger/model/dao/AccessorioDao.java
@@ -18,157 +18,109 @@ public class AccessorioDao {
 	}
 	
 	public List<Accessorio> getAllAccessori(){
-	    List<Accessorio> accessori = new ArrayList<Accessorio>();
-	    PreparedStatement pst = null;
-	    ResultSet rs = null;
-
-	    try {
-	        String query = "SELECT * FROM prodotto JOIN accessorio ON prodotto.codice = accessorio.codice";
-	        pst = this.con.prepareStatement(query);
-	        rs = pst.executeQuery();
-
-	        while (rs.next()) {
-	            Accessorio row = new Accessorio();
-	            row.setCodice(rs.getInt("codice"));
-	            row.setNome(rs.getString("nome"));
-	            row.setPrezzo(rs.getFloat("prezzo"));
-	            row.setMarca(rs.getString("marca"));
-	            row.setDescrizione(rs.getString("descrizione"));
-	            row.setDataUscita(rs.getDate("data_usc"));
-	            row.setTipo(rs.getString("tipo"));
-
-	            accessori.add(row);
-	        }
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    } finally {
-	        // Chiusura delle risorse nel blocco "finally"
-	        if (rs != null) {
-	            try {
-	                rs.close();
-	            } catch (Exception e) {
-	                e.printStackTrace();
-	            }
-	        }
-	        if (pst != null) {
-	            try {
-	                pst.close();
-	            } catch (Exception e) {
-	                e.printStackTrace();
-	            }
-	        }
-	    }
-
-	    return accessori;
+		List<Accessorio> accessori = new ArrayList<Accessorio>();
+		
+		try {
+			String query = "SELECT * FROM prodotto JOIN accessorio ON prodotto.codice = accessorio.codice";
+			PreparedStatement pst = this.con.prepareStatement(query);
+			ResultSet rs = pst.executeQuery();
+			while (rs.next()) {
+				Accessorio row = new Accessorio();
+				row.setCodice(rs.getInt("codice"));
+				row.setNome(rs.getString("nome"));
+				row.setPrezzo(rs.getFloat("prezzo"));
+				row.setMarca(rs.getString("marca"));
+				row.setDescrizione(rs.getString("descrizione"));
+				row.setDataUscita(rs.getDate("data_usc"));
+				row.setTipo(rs.getString("tipo"));
+				
+				accessori.add(row);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return accessori;
 	}
-
 	
-
+	public Accessorio getAccessorioByCodice(int codice){
+		Accessorio accessorio = null;
+		
+		try {
+			String query = "SELECT * FROM prodotto JOIN accessorio ON prodotto.codice = accessorio.codice WHERE accessorio.codice = ?";
+			PreparedStatement pst = this.con.prepareStatement(query);
+			pst.setInt(1, codice);
+			ResultSet rs = pst.executeQuery();
+			if (rs.next()) {
+				accessorio = new Accessorio();
+				accessorio.setCodice(rs.getInt("codice"));
+				accessorio.setNome(rs.getString("nome"));
+				accessorio.setPrezzo(rs.getFloat("prezzo"));
+				accessorio.setMarca(rs.getString("marca"));
+				accessorio.setDescrizione(rs.getString("descrizione"));
+				accessorio.setDataUscita(rs.getDate("data_usc"));
+				accessorio.setTipo(rs.getString("tipo"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return accessorio;
+	}
 	
 	public List<Accessorio> getAccessorioByPrezzo(int min, int max) {
-	    List<Accessorio> accessori = new ArrayList<Accessorio>();
-	    PreparedStatement pst = null;
-	    ResultSet rs = null;
-
+	    List<Accessorio> accessori = new ArrayList<Accessorio>();    
 	    try {
 	        String query = "SELECT * FROM prodotto JOIN accessorio ON prodotto.codice = accessorio.codice WHERE prezzo >= ? AND prezzo <= ?";
-	        pst = this.con.prepareStatement(query);
+	        PreparedStatement pst = this.con.prepareStatement(query);
 	        pst.setInt(1, min);
 	        pst.setInt(2, max);
-	        rs = pst.executeQuery();
-
+	        ResultSet rs = pst.executeQuery();
 	        while (rs.next()) {
-	            Accessorio row = new Accessorio();
-	            row.setCodice(rs.getInt("codice"));
-	            row.setNome(rs.getString("nome"));
-	            row.setPrezzo(rs.getFloat("prezzo"));
-	            row.setMarca(rs.getString("marca"));
-	            row.setDescrizione(rs.getString("descrizione"));
-	            row.setDataUscita(rs.getDate("data_usc"));
-	            row.setTipo(rs.getString("tipo"));
-
-	            accessori.add(row);
+	        	Accessorio row = new Accessorio();
+	        	row.setCodice(rs.getInt("codice"));
+	        	row.setNome(rs.getString("nome"));
+	        	row.setPrezzo(rs.getFloat("prezzo"));
+	        	row.setMarca(rs.getString("marca"));
+	        	row.setDescrizione(rs.getString("descrizione"));
+	        	row.setDataUscita(rs.getDate("data_usc"));
+	        	row.setTipo(rs.getString("tipo"));
+	        	
+	        	accessori.add(row);
 	        }
 	    } catch (Exception e) {
 	        e.printStackTrace();
-	    } finally {
-	        // Chiusura delle risorse nel blocco "finally"
-	        if (rs != null) {
-	            try {
-	                rs.close();
-	            } catch (Exception e) {
-	                e.printStackTrace();
-	            }
-	        }
-	        if (pst != null) {
-	            try {
-	                pst.close();
-	            } catch (Exception e) {
-	                e.printStackTrace();
-	            }
-	        }
 	    }
-
+	    
 	    return accessori;
 	}
-
 	
-
-	public void insertAccessorio(int codice, String tipo) {
-	    PreparedStatement statement = null;
-	    
-	    try {
-	        String query = "INSERT INTO accessorio (codice, tipo) VALUES (?, ?)";
-	        statement = this.con.prepareStatement(query);
-	        
-	        statement.setInt(1, codice);
-	        statement.setString(2, tipo);
-	        statement.executeUpdate();
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    } finally {
-	        // Chiusura della risorsa nel blocco "finally"
-	        if (statement != null) {
-	            try {
-	                statement.close();
-	            } catch (Exception e) {
-	                e.printStackTrace();
-	            }
-	        }
-	    }
-	}
-
+	public void insertAccessorio(int codice, String tipo ) {
+		  try {
+			  String query = "INSERT INTO accessorio (codice, tipo) VALUES (?, ?)";
+			  
+			  PreparedStatement statement = this.con.prepareStatement(query);			  
+			  statement.setInt(1, codice);
+			  statement.setString(2, tipo);
+			  statement.executeUpdate();
+	          statement.close();
+			  
+			
+			
+		  }	
+		  catch (Exception e) {
+		        e.printStackTrace();
+		        logger.info(e.getMessage());
+		    }
+		 
+		}
 	
 	public void deleteAccessorio(String codice) {
-	    PreparedStatement statement = null;
-
 	    try {
 	        String query = "DELETE FROM accessorio WHERE codice = ?";
-	        statement = this.con.prepareStatement(query);
-	        statement.setString(1, codice);
-	        statement.executeUpdate();
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    } finally {
-	        // Chiusura della risorsa nel blocco "finally"
-	        if (statement != null) {
-	            try {
-	                statement.close();
-	            } catch (Exception e) {
-	                e.printStackTrace();
-	            }
-	        }
-	    }
-	}
-
-
-	public void updateAccessorio(int codice, String tipo) {
-	    try {
-	        String query = "UPDATE accessorio SET tipo = ? WHERE codice = ?";
 	        
 	        PreparedStatement statement = this.con.prepareStatement(query);
-	        statement.setString(1, tipo);
-	        statement.setInt(2, codice);
+	        statement.setString(1, codice);
 	        
 	        statement.executeUpdate();
 	        statement.close();
@@ -176,8 +128,23 @@ public class AccessorioDao {
 	        e.printStackTrace();
 	        logger.info(e.getMessage());
 	    }
+	    
 	}
-
 	
-}
 
+
+	public void updateAccessorio(int codice, String tipo) {
+	    PreparedStatement statement = null;
+
+	    try {
+	        String query = "UPDATE accessorio SET tipo = ? WHERE codice = ?";
+	        statement = this.con.prepareStatement(query);
+	        statement.setString(1, tipo);
+	        statement.setInt(2, codice);
+	        statement.executeUpdate();
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        logger.info(e.getMessage());
+	    } 
+	}
+}
--- GameChanger/src/main/java/cn/gamechanger/model/dao/AmministratoreDao.java
+++ GameChanger/src/main/java/cn/gamechanger/model/dao/AmministratoreDao.java
@@ -66,5 +66,3 @@ public class AmministratoreDao {
 		return admin;
 	}
 }
-
-   
